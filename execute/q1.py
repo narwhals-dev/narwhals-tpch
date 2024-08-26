4ed53267552a202bec0a0680dba39e550869cce3
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-08-26 18:59:17 +0100

    simplify

--- execute/q1.py
+++ execute/q1.py
@@ -4,10 +4,24 @@ import os
 import pyarrow.parquet as pq
 import polars as pl
 import dask.dataframe as dd
+import pandas as pd
+
+pd.options.mode.copy_on_write = True
+pd.options.future.infer_string = True
 
 lineitem = os.path.join('data', 'lineitem.parquet')
 
-print(q1.query(pq.read_table(lineitem)))
-print(q1.query(pl.scan_parquet(lineitem)).collect())
-print(q1.query(dd.read_parquet(lineitem)).compute())
+IO_FUNCS = {
+    'pandas': lambda x: pd.read_parquet(x, engine='pyarrow'),
+    'pandas[pyarrow]': lambda x: pd.read_parquet(x, engine='pyarrow', dtype_backend='pyarrow'),
+    'polars[eager]': lambda x: pl.read_parquet(x),
+    'polars[lazy]': lambda x: pl.scan_parquet(x),
+    'pyarrow': lambda x: pq.read_table(x),
+    'dask': lambda x: dd.read_parquet(x, engine='pyarrow', dtype_backend='pyarrow'),
+}
+
+print(q1.query(IO_FUNCS['pandas[pyarrow]'](lineitem)))
+print(q1.query(IO_FUNCS['polars[lazy]'](lineitem)).collect())
+print(q1.query(IO_FUNCS['pyarrow'](lineitem)))
+print(q1.query(IO_FUNCS['dask'](lineitem)).compute())
 
